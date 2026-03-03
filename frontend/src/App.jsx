d5026f93228869b238d9b1ed39b9656ed6355646
virendra <virendrasuryawanshi901@gmail.com>
2026-03-03 12:10:43 +0530

    set up routing with React Router for login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {
+  BrowserROuter as Router,
+  Routes,
+  Route,
+}from "react-router-dom"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          
+        </Routes>
+      </Router>
+    </div>
   )
 }
 

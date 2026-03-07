b9f73ba4e896b7f72ef6e90eeeb4bdbf25408937
virendra <virendrasuryawanshi901@gmail.com>
2026-03-07 10:48:04 +0530

    Refactor code structure for improved readability and maintainability

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require ("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+//Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/reports", reportRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

b535d851c0891dfc53610363b18845c938bb8e01
virendra <virendrasuryawanshi901@gmail.com>
2026-03-12 22:36:27 +0530

    feat: import authentication routes in server setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(

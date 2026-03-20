02039dd489b771f1b502adbef6fa5e4ab9072b55
virendra <virendrasuryawanshi901@gmail.com>
2026-03-20 12:56:15 +0530

    fix: update middleware imports in report and user routes

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { adminOnly } = require("../middlewares/authMiddleware");
+const { adminOnly, protect } = require("../middlewares/authMiddleware");
 const {exportTasksReport, exportUsersReport} = require("../controllers/reportController")
+
 
 const router = express.Router();
 

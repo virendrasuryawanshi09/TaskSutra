65977a44f34982fd97875a93f9082d795a31c982
virendra <virendrasuryawanshi901@gmail.com>
2026-03-26 10:50:07 +0530

    feat: Implemented role-based SideMenu with navigation and logout support

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -3,65 +3,65 @@ import { UserContext } from '../../context/UserContextState'
 import { useNavigate } from 'react-router-dom';
 import { SIDE_MENU_DATA , SIDE_MENU_USER_DATA} from "../../utils/data"
 
-const SideMenu = ({ activeMenu }) => {
-  const { user, clearuser } = useContext(UserContext);
-  const [sideMenu, setSideMenu] = useState([]);
 
+const SideMenu = ({activeMenu}) => {
+
+  const {user, clearUser} = useContext(UserContext);
+  const[sideMenuData, setSideMenuData] = useState([]);
   const navigate = useNavigate();
 
   const handleClick = (route) => {
-    if (route === "logout") {
+    if(route === "logout") {
       handleLogout();
       return;
     }
+
     navigate(route);
   };
 
   const handleLogout = () => {
     localStorage.clear();
-    clearuser();
+    clearUser();
     navigate("/login");
   };
 
   useEffect(() => {
-    if (user) {
-      setSideMenuData(
-        user.role === 'admin' ? SIDE_MENU_DATA : SIDE_MENU_USER_DATA
-      );
+    if(user) {
+      setSideMenuData(user?.role === "admin" ? SIDE_MENU_DATA : SIDE_MENU_USER_DATA);
     }
-  }, [user]);
-
-  return (
+    return () => {};
+  }, [user])
+  return <div className="">
     <div className="">
-      <div className="">
-        <img
-          src={user?.profileImageUrl || ""}
-          alt="Profile Image"
-          className=''
-        />
-      </div>
-
-      {user?.role === "admin" && (
-        <div className="">
-          Admin
-        </div>
-      )}
-
-      <h5 className="">
-        {user?.name || ""}
-      </h5>
-
-      <p className="">{user?.email || ""}</p>
+      <img
+        src={user?.profileImageUrl || ""}
+        alt="Profile Image"
+        className=''
+      />
     </div>
 
-    {SideMenuData.map({item, index} =>
-      <button 
-        key={`menu_${index}`}
-        className="">
-        
-        </button>
+    {user?.role === "admin" && (
+      <div className="">
+        Admin
+      </div>
     )}
-  );
-};
 
-export default SideMenu;
+    <h5 className="">
+      {user?.name || ""}
+    </h5>
+
+    <p className="">{user?.email || ""}</p>
+  </div>
+
+  sideMenuData.map((item, index) => {
+    <button 
+      key={`menu_${index}`}
+      className={`w-full flex items-center gap-4 text-[15px] ${
+        activeMenu == item.label
+          ? "text-primary bg-linear-to-r from-blue-50/40 to-blue-100/50 border-r-3":""
+       } py-3 px-6 mb-3 curser-pointer` 
+      
+  })
+}
+
+export default SideMenu

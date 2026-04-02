173549aeaf3c019fbafdb1d2cce49ffb906b86b9
virendra <virendrasuryawanshi901@gmail.com>
2026-04-02 18:25:00 +0530

    feat: implement CustomBarChart component with dynamic color and tooltip for task priority visualization

--- frontend/src/components/Charts/CustomBarChart.jsx
+++ frontend/src/components/Charts/CustomBarChart.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Cell
+} from "recharts";
 
-const CustomBarChart = () => {
+const CustomBarChart = ({ data }) => {
+
+  const getBarColor = (entry) => {
+    switch (entry?.priority) {
+      case 'Low':
+        return '#4C7F6A';
+
+      case 'Medium':
+        return '#2F7A84';
+
+      case 'High':
+        return '#D97706';
+
+      default:
+        return '#8884d8';
+    }
+  };
+
+  const CustomTooltip = ({ active, payload }) => {
+    if(active && payload && payload.length) {
+      return (
+        <div className="">
+          <p className="">
+            {payload[0].payload.priority}
+          </p>
+          <p className="">
+            Count: {" "}
+            <span className="">
+              {payload[0].payload.count}
+            </span>
+          </p>
+        </div>
+      )
+    }
+    return null;
+  }
   return (
-    <div>CustomBarChart</div>
+    <div className="">
+      <ResponsiveContainer 
+    </div>
   )
 }
 

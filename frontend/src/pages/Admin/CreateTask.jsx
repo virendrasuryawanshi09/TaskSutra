63f42f49e27afc9f6459d05f6af00ac03b55cc3a
virendra <virendrasuryawanshi901@gmail.com>
2026-04-03 10:08:39 +0530

    feat: initialize create task page with state management and handlers

--- frontend/src/pages/Admin/CreateTask.jsx
+++ frontend/src/pages/Admin/CreateTask.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
+import DashboardLayout from '../../components/Layouts/DashboardLayout';
+import { PRIORITY_DATA } from '../../utils/data';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import toast from "react-hot-toast";
+import { useLocation, useNavigate } from "react-router-dom";
+import moment from "moment";
+import { LuTrash2 } from "react-icons/lu";
 
 const CreateTask = () => {
+
+  const location = useLocation();
+  const { taskId } = location.state || {};
+  const navigate = useNavigate();
+
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "",
+    priority: "Low",
+    dueDate: null,
+    assignedTo: [],
+    todoCheckList: [],
+    attachments: [],
+  });
+
+  const [currentTask, setCurrentTask] = useState(null);
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
+
+  const handleValueChange = (key, value) => {
+    setTaskData((prevData) => ({
+      ...prevData,
+      [key]: value,
+    }));
+  }
+
+  const clearData = () => {
+    setTaskData({
+      title: "",
+      description: "",
+      priority: "Low",
+      dueDate:null,
+      assignedTo: [],
+      todoCheckList: [],
+      attachments: [],
+    });
+  };
+
+  const createTask = async () => {};
+
+  const updateTask = async () => {};
+
+  const handleSubmit = async () => {};
+
+  const getTaskDetailsByID = async () => {};
+
+  const deleteTask = async () => {};
+
   return (
-    <div>CreateTask</div>
+    <DashboardLayout activeMenu="Create Task">
+
+    </DashboardLayout>
   )
 }
 

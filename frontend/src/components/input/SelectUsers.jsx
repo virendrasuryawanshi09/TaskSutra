39c47d4b7a6eb435c5c975be63fbc6946515d1c3
virendra <virendrasuryawanshi901@gmail.com>
2026-04-06 19:37:19 +0530

    feat: enhance SelectUsers component with user fetching and selection functionality; add Model component

--- frontend/src/components/input/SelectUsers.jsx
+++ frontend/src/components/input/SelectUsers.jsx
@@ -1,56 +1,62 @@
-import { useEffect, useState } from "react";
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import axiosInstance from "../../utils/axiosInstance";
-import { set } from "react-datepicker/dist/dist/date_utils.js";
+import { API_PATHS } from "../../utils/apiPaths";
+import { LuUsers } from "react-icons/lu";
 
-const SelectUsers = ({selectedUsers, setSelectedUsers}) => {
-
+const SelectUsers = ({ selectedUsers, setSelectedUsers }) => {
   const [allUsers, setAllUsers] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [tempSelectedUsers, setTempSelectedUsers] = useState([]);
 
   const getAllUsers = async () => {
     try {
-      const resopnse = await axiosInstance.get(API_PATHS.USERS.GET_ALLUSERS);
-      setAllUsers(resopnse.data);
-    }catch (error) {
+      const response = await axiosInstance.get(API_PATHS.USERS.GET_ALLUSERS);
+      setAllUsers(response.data);
+    } catch (error) {
       console.error("Error fetching users:", error);
-    };
+    }
   };
 
   const toggleUserSelection = (userId) => {
-    setTempSelectedUsers((prev) => {
-      prev.includes(userId) 
-      ? prev.filter((id) => id !== userId)
-      : [...prev, userId]
-    })
-  }
+    setTempSelectedUsers((prev) =>
+      prev.includes(userId)
+        ? prev.filter((id) => id !== userId)
+        : [...prev, userId]
+    );
+  };
 
   const handleAssign = () => {
     setSelectedUsers(tempSelectedUsers);
     setIsModalOpen(false);
-  }
+  };
 
   const selectedUserAvatars = allUsers
-  .filter((user) => selectedUsers.includes(user._id))
-  .map((user) => user.profileImageUrl);
+    .filter((user) => selectedUsers.includes(user._id))
+    .map((user) => user.profileImageUrl);
 
   useEffect(() => {
     getAllUsers();
   }, []);
 
   useEffect(() => {
-    if(selectedUsers.length === 0) {
+    if (selectedUsers.length === 0) {
       setTempSelectedUsers([]);
     }
-
-    return () => {};
   }, [selectedUsers]);
 
-  
   return (
-    <div>SelectUsers</div>
-  )
-}
+    <div>
+      {selectedUserAvatars.length === 0 && (
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="flex items-center gap-2 px-4 py-2 rounded-lg border border-gray-300 hover:bg-gray-100 transition"
+        >
+          <LuUsers className="text-lg" />
+          Add Members
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default SelectUsers
+export default SelectUsers;

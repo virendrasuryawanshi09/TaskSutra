1ca7b15a0bdc7bc98b5fc16dea6c3911b713e810
virendra <virendrasuryawanshi901@gmail.com>
2026-04-08 10:46:29 +0530

    feat: add getAllTasks function for task fetching and filtering

--- frontend/src/pages/Admin/ManageTasks.jsx
+++ frontend/src/pages/Admin/ManageTasks.jsx
@@ -1,16 +1,56 @@
-import React, {useEffect, useState} from 'react'
-import DashboardLayout from '../../components/Layouts/DashboardLayout'
+import React, { useEffect, useState } from 'react';
+import DashboardLayout from '../../components/Layouts/DashboardLayout';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 const ManageTasks = () => {
 
   const [allTasks, setAllTasks] = useState([]);
-
   const [tabs, setTabs] = useState([]);
-  const [filterStatus, setFilterStatus] = useState('all');
+  const [filterStatus, setFilterStatus] = useState('All');
 
   const navigate = useNavigate();
-  const getAllTasks = async () => {
 
+  const getAllTasks = async () => {
+    try {
+      const apiStatus =
+        filterStatus === 'All'
+          ? ''
+          : filterStatus === 'In Progress'
+            ? 'In-progress'
+            : filterStatus;
+
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {
+          status: apiStatus,
+        }
+      });
+
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+      const statusSummary = response.data?.statusSummary || {};
+
+      const statusArray = [
+        {
+          label: 'All', count: statusSummary.all || 0
+        },
+        {
+          label: "Pending", count: statusSummary.pendingTasks || 0 
+        },
+        {
+          label: "In Progress", count: statusSummary.inProgressTasks || 0 
+        },
+        {
+          label: "Completed", count: statusSummary.completedTasks || 0
+        }
+      ];
+
+      setTabs(statusArray);
+
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
   };
 
   const handleClick = (taskData) => {
@@ -23,7 +63,7 @@ const ManageTasks = () => {
 
   useEffect(() => {
     getAllTasks();
-    return () =>{};
+    return () => {};
   }, [filterStatus]);
 
   return (
@@ -32,7 +72,7 @@ const ManageTasks = () => {
 
       </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default ManageTasks
+export default ManageTasks;

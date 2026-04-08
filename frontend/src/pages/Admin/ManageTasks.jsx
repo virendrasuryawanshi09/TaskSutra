ca5f09569eb409035acc20fad715cc0520f48baa
virendra <virendrasuryawanshi901@gmail.com>
2026-04-08 10:19:00 +0530

    feat: setup ManageTasks page with filtering and navigation

--- frontend/src/pages/Admin/ManageTasks.jsx
+++ frontend/src/pages/Admin/ManageTasks.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import DashboardLayout from '../../components/Layouts/DashboardLayout'
 
 const ManageTasks = () => {
+
+  const [allTasks, setAllTasks] = useState([]);
+
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState('all');
+
+  const navigate = useNavigate();
+  const getAllTasks = async () => {
+
+  };
+
+  const handleClick = (taskData) => {
+    navigate(`/admin/create-task/`, { state: taskData._id });
+  };
+
+  const handleDownloadReport = async () => {
+
+  };
+
+  useEffect(() => {
+    getAllTasks();
+    return () =>{};
+  }, [filterStatus]);
+
   return (
-    <div>ManageTasks</div>
+    <DashboardLayout activeMenu="Manage Tasks">
+      <div className="">
+
+      </div>
+    </DashboardLayout>
   )
 }
 
